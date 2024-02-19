a45daeacd8aaa4379f708ea7aeef3087cd74cd0d
yanis59100 <meftouh.yanis@hotmail.com>
2024-02-19 16:39:08 +0100

    add 4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle, followed by a new line\n"); 
+	puts("\"Programming is like building a multilingual puzzle\n"); 
 	return 0;
 }

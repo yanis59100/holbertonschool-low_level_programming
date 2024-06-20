5fd5bc0e8cd8787732ccc56b84e5e77645fffebd
yanis59100 <meftouh.yanis@hotmail.com>
2024-06-20 09:09:10 +0200

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
    You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
    Your program should return 1
    Your program should compile without any warnings when using the -Wall gcc option

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
+int main(void)
+{
+		writ'("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

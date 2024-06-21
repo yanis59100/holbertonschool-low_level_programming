1f09784971b54d55dc45e40911407582fdad3873
yanis59100 <meftouh.yanis@hotmail.com>
2024-06-21 15:39:03 +0200

    Write a function that prints the n times table, starting with 0.
    Prototype: void print_times_table(int n);
    If n is greater than 15 or less than 0 the function should not print anything
    Format: see example

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_time_table - Prints the times table of the input,
- * $starting with 0.
- * Return: Always 0.
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
 void print_times_table(int n)
 {

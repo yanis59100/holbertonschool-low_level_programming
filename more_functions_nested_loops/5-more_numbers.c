562990eb6d19e655d2d2de482b1b6d63d72f4583
yanis59100 <meftouh.yanis@hotmail.com>
2024-06-24 11:59:59 +0200

    Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
    Prototype: void more_numbers(void);
    You can only use _putchar three times in your code

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 	int x;
 	int y;
 
-	for (x = '0'; x < 10; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = '0'; y < 15; y++)
+		for (y = 0; y < 15; y++)
 		{
 			if (y > 9)
-				_putchar(y / 10 + '0');
+			_putchar(y / 10 + '0');
 			_putchar(y % 10 + '0');
 		}
 		_putchar('\n');

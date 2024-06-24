1f4542a0ff6ca8f786da6a678054ab26110371e6
yanis59100 <meftouh.yanis@hotmail.com>
2024-06-24 10:57:47 +0200

    Modifying

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,3 +13,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 
+#endif

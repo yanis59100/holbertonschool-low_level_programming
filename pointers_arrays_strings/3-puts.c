117cce0db35219b3372e857c86c21e6c865f8cbe
yanis59100 <meftouh.yanis@hotmail.com>
2024-06-25 12:41:44 +0200

    Write a function that prints a string, followed by a new line, to stdout.
    Prototype: void _puts(char *str);
    FYI: The standard library provides a similar function: puts. Run man puts to learn more.

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * _puts - returns the length of a string.
+ * @str: input string.
+ * Return: length of a string.
+ */
+void _puts(char *str)
+{
+	int x = 0;
+
+	while (*(str + x) != '/0')
+		x++
+}

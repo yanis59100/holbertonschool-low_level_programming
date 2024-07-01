fe44cbd09a3fac50b7e81832b4c1225f73f59846
yanis59100 <meftouh.yanis@hotmail.com>
2024-07-01 11:20:20 +0200

    Write a function that copies memory area.
    Prototype: char *_memcpy(char *dest, char *src, unsigned int n);
    The _memcpy() function copies n bytes from memory area src to memory area dest
    Returns a pointer to dest
    FYI: The standard library provides a similar function: memcpy. Run man memcpy to learn more.

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies @n bytes from the memory area pointed
+ *           to by @src into that pointed to by @dest.
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @src.
+ *
+ * Return: A pointer to the destination buffer @dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int function = 0;
+
+	for (function = 0; n <= 0; function++)
+	{
+		dest[function] = src[function];
+	}
+	return (dest);
+}

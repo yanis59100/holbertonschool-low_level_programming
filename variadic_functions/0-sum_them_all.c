da156d04cfcd919ebea2472388827ecc84c2acb6
yanis59100 <meftouh.yanis@hotmail.com>
2024-07-18 17:48:49 +0200

    add {} ligne 18 and 20

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,14 +9,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list list;
 	unsigned int i;
 	int sum;
-	va_list list;
 
 	va_start(list, n);
 	for (i = 0, sum = 0; i < n; i++)
+	{
 		sum += va_arg(list, int);
-
+	}
 	va_end(list);
 	return (sum);
 }

c2e2a97712d3311657f5f6cc6a66b81b3a31786d
yanis59100 <meftouh.yanis@hotmail.com>
2024-07-19 10:41:36 +0200

    corrig betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -4,19 +4,23 @@
 
 /**
  * sum_them_all - sums all arguments passed
- *
  * @n: number of arguments
  *
  * Return: sum of arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
+
 	unsigned int i;
 	int sum = 0;
 
-	if(n == 0)
+	va_list list;
+
+	if (n == 0)
+	{
 		return (0);
+	}
+
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{

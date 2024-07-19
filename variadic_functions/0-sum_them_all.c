a380d425c75484495e0a8730f23b0207a3b4daf1
yanis59100 <meftouh.yanis@hotmail.com>
2024-07-19 10:35:41 +0200

    correction de la boucle for

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,12 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
-	int sum;
+	int sum = 0;
 
 	if(n == 0)
 		return (0);
 	va_start(list, n);
-	for (i = 0, sum = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}

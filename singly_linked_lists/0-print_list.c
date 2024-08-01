2b2e11027b43866d9711f96c122644e8959f894a
yanis59100 <meftouh.yanis@hotmail.com>
2024-08-01 16:01:14 +0200

    add {} after the if and else

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,9 +12,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		i++;
 	}

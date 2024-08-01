c3cf1c055397c600728a98e5f0d1493b4297b500
yanis59100 <meftouh.yanis@hotmail.com>
2024-08-01 15:24:54 +0200

    Modifying variable int by unsigned int

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,16 +10,16 @@
 
 size_t print_list(const list_t *h)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (h != NULL)
 	{
-		if(h->str == NULL)
-			printf("[0] (nil) \n");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s \n", h->len, h->str);
-		i++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		i++;
 	}
 	return (i);
 }

459066c3b00b89ec359e0688923031b5b2ef8a1d
yanis59100 <meftouh.yanis@hotmail.com>
2024-08-01 15:26:17 +0200

    adding an s on LISTS_S

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 typedef struct list_s
 {
@@ -8,7 +8,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/*Prototypes*/
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);

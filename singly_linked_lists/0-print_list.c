3ab97a3f5482402135daba24800c36d3c4fc713d
yanis59100 <meftouh.yanis@hotmail.com>
2024-08-01 15:47:33 +0200

    Problem with betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * print_listint - Prints all elements of a list
+ * print_list - Prints all elements of a list
  * @h: Pointer to a list
- *
  * Return: Number of nodes
  */
 
@@ -14,7 +12,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil) \n");
 		else
 			printf("[%d] %s \n", h->len, h->str);

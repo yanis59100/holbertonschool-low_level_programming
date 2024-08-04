131daadde5d1ec4d3d4a8076c464b330a0f06441
yanis59100 <meftouh.yanis@hotmail.com>
2024-08-04 18:43:21 +0200

    Write a function that reads a text file and prints it to the POSIX standard output.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a file and print it in stdout
+ * @filename: path of file
+ * @letters: number of letter to print
+ * Return: number of letter it can read, 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int o_read, o_w, fd;
+
+	o_read = o_w = fd = 0;
+	if (filename)
+	{
+		fd = open(filename, O_RDONLY);
+		if (fd != -1)
+		{
+			buf = malloc(letters);
+			if (buf)
+			{
+				o_read = read(fd, buf, letters);
+				if (o_read != -1)
+					o_w = write(STDOUT_FILENO, buf, o_read);
+				free(buf);
+			}
+			close(fd);
+		}
+	}
+	if (fd == -1 || o_read == -1 || o_w == -1)
+		return (0);
+	return (o_w);
+}
---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: input
+ * @text_content: input msg
+ * Return: 1 is success, -1 fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, o_wr = 0, isok = -1;
+
+	if (!filename)
+		return (isok);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd != -1)
+	{
+		if (text_content)
+		{
+			while (text_content[len])
+				len++;
+			o_wr = write(fd, text_content, len);
+		}
+		close(fd);
+		if (o_wr != -1)
+			isok = 1;
+	}
+	return (isok);
+}
